109106beae58edad7b0cafd18108477b2399c103
emilie-ponce <emilie.ponce@laplateforme.io>
2025-03-27 13:10:13 +0100

    changement des dates en version française

--- js/planning.js
+++ js/planning.js
@@ -16,21 +16,26 @@ document.getElementById('ajouter').addEventListener('click', (e) => {
 
     const recetteSelect = document.getElementById('recette');
     const recette = recetteSelect.options[recetteSelect.selectedIndex].text;
-    const date = document.getElementById('date').value;
+    const dateInput = document.getElementById('date').value; 
     const message = document.getElementById('message');
-    const today = new Date().toISOString().split('T')[0];
 
-    if (!date) {
+    if (!dateInput) {
         message.textContent = "Veuillez choisir une date.";
         message.style.color = "red";
         return;
     }
 
+    const formattedDate = new Date(dateInput).toLocaleDateString('fr-FR', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    });
+
     const planningTable = document.getElementById('planning');
     const planningBody = planningTable.querySelector('tbody');
     const newRow = document.createElement('tr');
     newRow.innerHTML = `
-        <td class="px-4 py-2 border border-blue-950">${date}</td>
+        <td class="px-4 py-2 border border-blue-950">${formattedDate}</td>
         <td class="px-4 py-2 border border-blue-950">${recette}</td>
     `;
     planningBody.appendChild(newRow);
