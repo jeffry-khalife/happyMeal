335633352cbdd85801565e1aaea4c64c8fcdb450
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-04-04 09:52:33 +0200

    Modification de l'affichage des favoris

--- js/favoris.js
+++ js/favoris.js
@@ -6,20 +6,22 @@ function chargerFavoris() {
 
     favoris.forEach((recette, index) => {
     const recetteDiv = document.createElement('article');
-    recetteDiv.className = 'sm:grid grid-cols-5 bg-white shadow-sm p-7 relative lg:max-w-2xl sm:p-4 rounded-lg lg:col-span-2 lg:ml-20 mb-6';
+    recetteDiv.className = 'recette bg-white p-4 rounded-lg shadow-md';
     recetteDiv.innerHTML = `
-        <img src="${recette.image}" alt="${recette.nom}" class="w-full rounded-lg">
+        <img src="${recette.image}" alt="${recette.nom}" class="w-full h-40 object-cover rounded-lg mb-4">
         <div class="pt-5 self-center sm:pt-0 sm:pl-10 col-span-3">
             <h2 class="text-gray-800 capitalize text-xl font-bold">${recette.nom}</h2>
             <p class="text-sm text-gray-600 mt-2">${recette.categorie}</p>
         </div>
         <div class="justify-self-end">
-            <button onclick="supprimerFavori(${index})" class="text-red-500 hover:text-red-700 text-lg">
+            <button onclick="supprimerFavori(${index})" class="text-black hover:text-red-500 text-lg">
             Supprimer
             </button>
         </div>
         `;
-
+        recetteDiv.addEventListener('click', function() {
+            ouvrirModal(recette);
+        });
         container.appendChild(recetteDiv);
         });
 }
