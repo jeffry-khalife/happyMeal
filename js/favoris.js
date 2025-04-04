9397a7a82cc97da80c3ba5b86d2b4b89e5224118
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-04-04 11:45:03 +0200

    Modification de l'affichage des favoris

--- js/favoris.js
+++ js/favoris.js
@@ -13,10 +13,13 @@ function chargerFavoris() {
             <h2 class="text-gray-800 capitalize text-xl font-bold">${recette.nom}</h2>
             <p class="text-sm text-gray-600 mt-2">${recette.categorie}</p>
         </div>
+        <div class="flex justify-between items-center">
+        <button class="bg-green-500 text-white rounded-full px-4 py-2 hover:bg-green-700 transition">Voir plus</button><br>
         <div class="justify-self-end">
             <button onclick="supprimerFavori(${index})" class="text-black hover:text-red-500 text-lg">
             Supprimer
             </button>
+        </div>
         </div>
         `;
         recetteDiv.addEventListener('click', function() {
