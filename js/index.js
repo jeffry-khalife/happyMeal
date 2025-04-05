265e6b5f92e398a7aa6a77b73da0030911e23fdc
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-04-05 14:05:23 +0200

    Ajout du listener pour ouvrir le modal avec les recettes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-fetch('../json/data.json')
+fetch('json/datas.json')
 .then(response => response.json())
 .then(data => {
+
     const recipesContainer = document.getElementById('random-recipes-container');
 
     // Mélangez les recettes pour un affichage aléatoire
@@ -14,13 +15,16 @@ fetch('../json/data.json')
         recetteDiv.className = 'recette bg-grey-200 p-4 rounded-lg shadow-md';
 
         recetteDiv.innerHTML = `
-            <a href="./recettes.html?id=${recette.id}">
-                <img src="${recette.image}" alt="${recette.nom}" class="w-full h-40 object-cover rounded-lg mb-4">
-            </a>
+            <img src="${recette.image}" alt="${recette.nom}" class="w-full h-40 object-cover rounded-lg mb-4">
             <h2 class="text-lg font-bold text-green-500 mb-2">${recette.nom}</h2>
             <p class="text-sm text-gray-600 mb-2">Catégorie : ${recette.categorie || 'Non spécifiée'}</p>
             <p class="text-sm text-gray-600 mb-2">Temps de préparation : ${recette.temps_preparation || 'Non spécifié'}</p>
         `;
+
+        recetteDiv.addEventListener('click', function() {
+            ouvrirModal(recette);
+        });
+
         recipesContainer.appendChild(recetteDiv);
     });
 })
